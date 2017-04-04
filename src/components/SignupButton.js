45ddf8920b6946c008ffb9a6da5bf2f0ef47d8a0
Mo-Sho <muhama94@gmail.com>
2017-04-04 14:59:40 -0700

    initial commit

---
+++ src/components/SignupButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignupButton extends Component {
+  render() {
+    return (
+      <button>SIGNUP</button>
+    );
+  }
+}
+
+export default SignupButton;
